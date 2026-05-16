0d075a801cc30ba97fece8b717abe2a9d1dc8ece
Sayan Senapati <sayansenapati2544@gmail.com>
2026-05-16 06:16:31 +0530

    docs: include final benchmark results and update basic.js verification script

--- basic.js
+++ basic.js
@@ -9,10 +9,6 @@ app.get('/about', (req, res) => {
   res.send('About World');
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
